59ab978d5536d0e5e6acb5f199c703731281f32a
MrGoddar <134316279+MrGoddar@users.noreply.github.com>
2026-05-27 19:03:10 -0300

    first commit

---
+++ cypress/e2e/login.spec.cy.js
@@ -0,0 +1,28 @@
+describe('Orange HRM Tests', () => {
+
+  const selectorsList = {
+    usernameField: "[name = 'username']",
+    passwordField:"[name = 'password']",
+    loginButton: '.oxd-button',
+    sectionTitleTopBar: '.oxd-topbar-header-title',
+    wrongCredentialAlert: '.oxd-alert'
+  }
+
+  it('login com sucesso', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get(selectorsList.usernameField).type('Admin')
+    cy.get(selectorsList.passwordField).type('admin123')
+    cy.get(selectorsList.loginButton).click()
+    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
+    cy.get(selectorsList.sectionTitleTopBar).contains('Dashboard')
+  })
+
+  it('login com falha', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get(selectorsList.usernameField).type('Teste')
+    cy.get(selectorsList.passwordField).type('Teste')
+    cy.get(selectorsList.loginButton).click()
+    cy.get(selectorsList.wrongCredentialAlert)
+  })
+
+})
