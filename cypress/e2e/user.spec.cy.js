82a179387f7c48366e37c67fb0348bdabe1c7746
MrGoddar <134316279+MrGoddar@users.noreply.github.com>
2026-06-08 15:20:25 -0300

    User update scenario

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -7,18 +7,35 @@ describe('Orange HRM Tests', () => {
     loginButton: '.oxd-button',
     sectionTitleTopBar: '.oxd-topbar-header-title',
     dashboardGrid: '.orangehrm-dashboard-grid > :nth-child(1)',
-    wrongCredentialAlert: '.oxd-alert'
+    wrongCredentialAlert: '.oxd-alert',
+    myInfoButton: ':nth-child(6) > .oxd-main-menu-item',
+    firstNameField: '[name="firstName"]',
+    lastNameField: '[name="lastName"]',
+    genericField: ".oxd-input--active",
+    
   }
 
 
 
-  it('login com sucesso', () => {
+  it.only('User info update - Success', () => {
     cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
     cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
     cy.get(selectorsList.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
     cy.get(selectorsList.dashboardGrid)
+    cy.get(selectorsList.myInfoButton).click()
+    cy.get(selectorsList.firstNameField).clear().type('FirstNameTest')
+    cy.get(selectorsList.lastNameField).clear().type('LastNameTest')
+    cy.get(selectorsList.genericField).eq(3).clear().type('IdTest')
+    cy.get(selectorsList.genericField).eq(4).clear().type('OtherIdTest')
+    cy.get(selectorsList.genericField).eq(5).clear().type('05688')
+    cy.get(selectorsList.genericField).eq(6).clear().type('2025-24-06')
+    cy.get(':nth-child(1) > .oxd-form > .oxd-form-actions > .oxd-button').click()
+    cy.get('body').should('contain', 'Successfully Updated')
+    
+    
+
   })
 
   it('login com falha', () => {
